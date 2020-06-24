97cb2331f6533b88a592426182c4cb8e05686d01
Vivekjain1503 <54208908+Vivekjain1503@users.noreply.github.com>
2020-06-24 14:42:28 +0530

    Update queryprocessor.py

--- queryprocessor.py
+++ queryprocessor.py
@@ -4,7 +4,12 @@ def process_query(query):
 	if ("romeo and juliet" in query.lower()):
 		return "William Shakespeare is not the answer"
 	if ("plus" in query.lower()):
-		num1 = int(query.split()[2])
-		num2 = int(query.split()[4])
+		num1 = int(query.split()[3])
+		num2 = int(query.split()[5])
+		# print(num1,num2)
 		return str(num1+num2)
+	# if ("largest" in query.lower())
+	# 	nums = query.split(":")
 	return ""
+
+# print(process_query("what is 20 plus 5"))
