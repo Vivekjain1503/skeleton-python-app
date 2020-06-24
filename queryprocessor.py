cbbe5543a52b6b3bbc4b87ae2403f0e15f9fbbeb
Vivekjain1503 <54208908+Vivekjain1503@users.noreply.github.com>
2020-06-24 14:22:52 +0530

    Update queryprocessor.py

--- queryprocessor.py
+++ queryprocessor.py
@@ -1,8 +1,8 @@
 def process_query(query):
-	if ("romeo and juliet" in query.lower()):
-		return "William Shakespeare is not the answer"
 	if ("name" in query.lower()):
 		return "Vivek"
+	if ("romeo and juliet" in query.lower()):
+		return "William Shakespeare is not the answer"
 	if ("email" in query.lower()):
 		return "vivek-c.jain@db.com"
 	return ""
