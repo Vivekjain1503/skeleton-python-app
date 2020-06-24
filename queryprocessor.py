493ef79868d0870a9560863bc413334b1c2f21b4
Vivekjain1503 <54208908+Vivekjain1503@users.noreply.github.com>
2020-06-24 15:17:23 +0530

    Update queryprocessor.py

--- queryprocessor.py
+++ queryprocessor.py
@@ -23,6 +23,20 @@ def process_query(query):
 		num2 = int(query.split()[-4])
 		# print(num1,num2)
 		return str(num1*num2)
+	if ("both a square and a cube"):
+		nums = query.split(":")[2].strip()
+		nums = nums.split(",")
+		print(nums)
+		a = 0
+		for num in nums:
+			a = int(num)
+			x = a ** (1 / 3)
+			x = int(round(x))
+			y = a ** (1 / 2)
+			y = int(round(y))
+			ans = True
+			if x**3 == a and y**2 == a:
+				return str(a)
 	return ""
 
 # print(process_query("what is 20 plus 5"))
