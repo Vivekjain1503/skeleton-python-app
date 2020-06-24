de451277700093b7cec71ab7bdc327e52c4f8843
Vivekjain1503 <54208908+Vivekjain1503@users.noreply.github.com>
2020-06-24 14:00:46 +0530

    Added a query

--- queryprocessor.py
+++ queryprocessor.py
@@ -1,4 +1,6 @@
 def process_query(query):
 	if ("romeo and juliet" in query.lower()):
 		return "William Shakespeare is not the answer"
+	if ("what is your name" in query.lower()):
+		return "Vivek"
 	return ""
