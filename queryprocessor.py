62524968abb283ba5107e909266a35a833b325de
Vivekjain1503 <54208908+Vivekjain1503@users.noreply.github.com>
2020-06-24 14:44:50 +0530

    Update queryprocessor.py

--- queryprocessor.py
+++ queryprocessor.py
@@ -8,8 +8,14 @@ def process_query(query):
 		num2 = int(query.split()[5])
 		# print(num1,num2)
 		return str(num1+num2)
-	# if ("largest" in query.lower())
-	# 	nums = query.split(":")
+	if ("largest" in query.lower())
+		nums = query.split(":")[1].trim()
+		nums = nums.split(",")
+		a = 0
+		for num in nums:
+			if int(num) > a :
+				a = int(num)
+		return str(a)
 	return ""
 
 # print(process_query("what is 20 plus 5"))
